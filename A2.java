864ff83fe1146bce6377346482fe47e3d10dca41
PowerRob <90591052+PowerRob@users.noreply.github.com>
2021-09-12 23:50:11 -0400

    Add files via upload

---
+++ A2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class A2 {
+    /**
+     * This function reads the grades from the given filename and returns
+     * them as an array of integers.
+     * 
+     * @param filename the filename to read
+     * @return an array of grades
+     */
+    public static int[] loadGradesFromFile(String filename) {
+        int[] grades = null;
+        
+        // TODO:
+        // Load the grades from the file and return them.
+        // 1. Read the number of grades N from the file.
+        // 2. Create an integer array of size N.
+        // 3. Loop through the rest of the file (N values), storing
+        //    each in the appropriate element of the array.
+        
+        
+
+        
+        return grades;
+    }
+
+    public static int[] createBins(int[] data, int[] upperBound) {
+        int[] bin = null;
+        
+        // TODO:
+        // Create the bin array and fill it with the appropriate counts.
+        // The size of the bin array should be the same as the size of the
+        // upperBound array. And data element <= to an upper bound should
+        // fall in that bin.
+        
+        
+        return bin;
+    }
+
+}
